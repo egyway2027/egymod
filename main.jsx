a34bb82b46d7fa199c3ec4ceace210191bcf3719
egyway2027 <egyway2027@gmail.com>
2026-08-02 00:12:39 +0300

    Delete main.jsx

--- main.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import ReactDOM from 'root'
-import App from './App.jsx'
-
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
