d3c2cfb7d17f480fc5793b2673dc9cb2cb0ade42
egyway2027 <egyway2027@gmail.com>
2026-08-04 02:47:11 +0300

    Update supabaseClient.js

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -1,7 +1,7 @@
 import { createClient } from "@supabase/supabase-js";
 
 // ضع رابط مشروعك الحقيقي ومفتاح anon الخاص بك مكان النصوص المشار إليها
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL || "https://jvmowzfktfybjcvqnlcc.supabase.co/rest/v1/";
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL || "https://jvmowzfktfybjcvqnlcc.supabase.co/rest";
 const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY || "sb_publishable_lqpryj6bARHXiqDveRUrVw_scmwGO-0";
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
