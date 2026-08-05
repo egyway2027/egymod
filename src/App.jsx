35f7a61969efa8a7a6a783fe3cd5f63d252d50d5
egyway2027 <egyway2027@gmail.com>
2026-08-05 08:54:06 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -278,11 +278,10 @@ const netProfit = useMemo(() => {
                         } else if (b.key === "whatsapp") {
                           setShowWhatsAppModal(true);
                         } else if (b.key === "deleteClient") {
-                          setShowRecycleBinModal(true);
+                         setShowRecycleBinModal(true);
+                        } else if (b.key === "deleteClient") {
+                          navigateTo("deleteClient");
                         }
-                     } else if (b.key === "deleteClient") {
-  navigateTo("deleteClient");
-}
                       }}
                       style={{
                         display: "flex", alignItems: "center", justifyContent: "space-between",
