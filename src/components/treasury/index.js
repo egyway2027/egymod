806bc1ca65fac22977a34fe9f83a9bba0c06695a
egyway2027 <egyway2027@gmail.com>
2026-08-07 18:22:37 +0300

    Create index.js

---
+++ src/components/treasury/index.js
@@ -0,0 +1,6 @@
+// src/components/treasury/index.js
+export { TreasuryMainScreen } from "./TreasuryMainScreen";
+export { ExpensesScreen } from "./ExpensesScreen";
+export { EmployeesMergedScreen } from "./EmployeesMergedScreen";
+export { PartnersScreen } from "./PartnersScreen";
+export { ProfitDistributionScreen } from "./ProfitDistributionScreen";
